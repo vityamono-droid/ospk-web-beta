c10922bb2285bd6dfb48bde2d2f3c7addb73d984
EmptyChapter <sakamuraniku@gmail.com>
2026-03-04 15:58:02 +0500

    Role management type shit

--- packages/frontend/src/pages/admin/services/services.router.tsx
+++ packages/frontend/src/pages/admin/services/services.router.tsx
@@ -2,10 +2,13 @@ import { lazy } from 'react'
 import { Outlet, Route, Routes } from 'react-router'
 import TabbedLayout from '../tabbed.layout'
 
+import { useAuthContext } from '@pages/auth/auth.context'
+
 const ServiceListPage = lazy(() => import('./serviceList'))
 const CatalogListPage = lazy(() => import('./catalogList'))
 const CategoryListPage = lazy(() => import('./categoryList'))
 const ServicePage = lazy(() => import('./servicePage'))
+const Page404 = lazy(() => import('@pages/errors/error404.page'))
 
 const tabs = [
   {
@@ -24,16 +27,24 @@ const tabs = [
 
 /** /admin/services */
 const ServicesRouter = () => {
+  const { account } = useAuthContext()
+
   return (
     <>
       <Routes>
-        <Route element={<TabbedLayout tabs={tabs} />}>
-          <Route index element={<ServiceListPage />} />
-          <Route path={'catalogs'} element={<CatalogListPage />} />
-          <Route path={'categories'} element={<CategoryListPage />} />
-        </Route>
-        <Route path={'new'} element={<ServicePage />} />
-        <Route path={':id'} element={<ServicePage />} />
+        {['admin', 'moder'].some((item) => account?.roles.includes(item)) ? (
+          <>
+            <Route element={<TabbedLayout tabs={tabs} />}>
+              <Route index element={<ServiceListPage />} />
+              <Route path={'catalogs'} element={<CatalogListPage />} />
+              <Route path={'categories'} element={<CategoryListPage />} />
+            </Route>
+            <Route path={'new'} element={<ServicePage />} />
+            <Route path={':id'} element={<ServicePage />} />
+          </>
+        ) : (
+          <Route index element={<Page404 />} />
+        )}
       </Routes>
       <Outlet />
     </>
