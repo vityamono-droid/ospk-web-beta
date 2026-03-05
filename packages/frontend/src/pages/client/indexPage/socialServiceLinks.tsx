08be7e90d733815315fd23d5f5c76aa25c674a85
EmptyChapter <sakamuraniku@gmail.com>
2026-03-05 19:00:15 +0500

    fucking hell

--- packages/frontend/src/pages/client/indexPage/socialServiceLinks.tsx
+++ packages/frontend/src/pages/client/indexPage/socialServiceLinks.tsx
@@ -4,46 +4,53 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import IndexHeader from './index.header'
 
+import MinzdravRf from '@assets/minzdrav_rf.png'
+import SlugKrovi from '@assets/slug_krovi.png'
+import Gubernator from '@assets/gubernator74.png'
+import Minzdrav74 from '@assets/minzdrav74.png'
+import Zashpravpotreb from '@assets/zashpravpotreb.png'
+import Roszdravnadzor74 from '@assets/roszdravnadzor74.png'
+
 const services = [
   {
     label: 'Министерство здравоохранения Российской Федерации',
-    image: 'minzdrav_rf.png',
+    image: MinzdravRf,
     link: 'https://www.rosminzdrav.ru/',
   },
   {
     label: 'Служба крови',
-    image: 'slug_krovi.png',
+    image: SlugKrovi,
     link: 'http://donorsapiens.yadonor.ru/',
   },
   {
     label: 'Губернатор Челябинской Области',
-    image: 'gubernator74.png',
+    image: Gubernator,
     link: 'http://gubernator74.ru/',
   },
   {
     label: 'Правительство Челябинской Области',
-    image: 'minzdrav74.png',
+    image: Minzdrav74,
     link: 'http://pravmin74.ru/',
   },
   {
     label: 'Министерство здравоохранения Челябинской Области',
-    image: 'minzdrav74.png',
+    image: Minzdrav74,
     link: 'http://www.zdrav74.ru/',
   },
   {
     label: 'Министерство социальных отношений Челябинской Области',
-    image: 'zashpravpotreb.png',
+    image: Zashpravpotreb,
     link: 'http://minsoc74.ru/',
   },
   {
     label: 'Территориальный орган роспотребнадзора по Челябинской Области',
-    image: 'minzdrav74.png',
+    image: Minzdrav74,
     link: 'http://74reg.roszdravnadzor.ru/',
   },
   {
     label:
       'Управление федеральной службы по надзору в сфере защиты прав потребителей и благополучия человека по Челябинской Области',
-    image: 'roszdravnadzor74.png',
+    image: Roszdravnadzor74,
     link: 'http://74.rospotrebnadzor.ru/',
   },
 ]
@@ -57,7 +64,7 @@ const SocialServiceLinks = () => {
           <Grid key={item.link} size={{ xs: 6, lg: 3 }} component={Link} href={item.link} underline={'hover'}>
             <Stack spacing={2}>
               <Stack height={132} direction={'row'} alignItems={'center'} justifyContent={'center'}>
-                <img height={80} src={`src/assets/${item.image}`} />
+                <img height={80} src={item.image} />
               </Stack>
               <Stack p={1} height={132} bgcolor={'divider'} textAlign={'center'} justifyContent={'center'}>
                 <Typography variant={'body2'} textTransform={'uppercase'}>
